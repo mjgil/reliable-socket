76ee91e07a1c2f424183953c05f6637ea7b89fc3
Malcom Gilbert <malcomgilbert@gmail.com>
2013-06-03 11:54:23 -0400

    refactoring and adding more tests

--- test/serverside.js
+++ test/serverside.js
@@ -17,12 +17,26 @@ describe('reliable-socket', function() {
   describe('reliable-socket-instance', function() {
     var reliableConstruct = rsock(websocket),
         uri = 'ws://localhost:8000',
-        server = websocket.Server({port: 8000});
+        server = websocket.Server({port: 8000}),
+        socketInstance = reliableConstruct(uri);
 
     it('should have all of the correct parameters', function() {
-      var socketInstance = reliableConstruct(uri);
-      
+      expect(socketInstance.uri).to.be(uri);
+      expect(socketInstance.reconnectTimeout).to.be(5000);
+      expect(socketInstance.retryTimeout).to.be(1000);
+      expect(socketInstance.seenObj).to.be.an('object');
+      expect(socketInstance.lastSeen).to.be(0);
+      expect(socketInstance.firstOpen).to.be(true);
+      expect(socketInstance.firstClosed).to.be(true);
+      expect(socketInstance.send).to.be.a('function');
+      expect(socketInstance.write).to.be.a('function');
+      expect(socketInstance.setupSocketListeners).to.be.a('function');
+      expect(socketInstance.tryToReopen).to.be.a('function');
+      expect(socketInstance.checkStillClosed).to.be.a('function');
+      expect(socketInstance.handleData).to.be.a('function');
     });
+
+
 
   });
 });
