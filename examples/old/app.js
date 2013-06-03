9a36ded00e3d67ad74b974bf4d33de84d6a578a7
Malcom Gilbert <malcomgilbert@gmail.com>
2013-06-03 11:12:52 -0400

    adding tests and examples

---
+++ examples/old/app.js
@@ -0,0 +1,71 @@
+var express = require('express');
+
+// Require engine io from the symlinked project
+// This guarantees on the server we will be using our
+// development version.
+var reliableServer = require('./reliable-socket-server')();
+console.log(reliableServer);
+var engineio = require('engine.io');
+
+var client = require('./node_modules/engine.io-client1');
+
+var fs = require('fs');
+// Create a new express app
+var app = express();
+
+// Create a new HTTP server listening on port 8000
+var server = app.listen(8000);
+
+// Create an Engine.IO instance on this server
+var io = engineio.attach(server);
+
+// Return the index.html file for the '/' path
+app.get('/', function (req, res) {
+  res.sendfile(__dirname + '/index.html');
+});
+
+// Return the engine io client javascript
+// ** NOTE
+// ** Notice how the actual file we're returning is the
+// ** development version of the file inside the engine.io-client
+// ** folder. This subfolder is really a symlink (linux/mac) or
+// ** shortcut (windows)
+app.get('/engine.io-client/engine.io.js', function (req, res) {
+  res.sendfile(__dirname + '/node_modules/engine.io-client1/engine.io.js');
+});
+
+// Here's where the actual Engine.IO server code comes in.
+// Here we tell it what to do.
+
+io.on('connection', function (socket) {
+    console.log('io: ', io);
+    reliableServer.addSocket(socket);
+    var stream = fs.createReadStream('bug2.txt');
+    socket.on('close', function (reason, desc) {
+        stream.destroy();
+    });
+
+    socket.on('error', function(error) {
+        console.log(error);
+    });
+
+    socket.on('retry', function(err) {
+      console.log('retry');
+    });
+
+    stream.on('data', sendData);
+
+    socket.on('data', function(data) {
+        console.log(data);
+    });
+
+    function sendData(data) {
+        socket.send(data);
+        setTimeout(function() {
+            sendData(data);
+        }, 2500);
+        // var newStream = fs.createReadStream('bug_106.txt');
+        // newStream.on('data', sendData);
+    }
+});
+
---
+++ test/serverside.js
@@ -0,0 +1,28 @@
+var rsock = require('../index'),
+  expect = require('expect.js'),
+  websocket = require('ws');
+
+
+
+describe('reliable-socket', function() {
+  it('should be a function', function() {
+    expect(rsock).to.be.a('function');
+  });
+
+  it ('should return reliable socket constructor', function() {
+    var reliableConstruct = rsock(websocket);
+    expect(reliableConstruct).to.be.a('function');
+  });
+
+  describe('reliable-socket-instance', function() {
+    var reliableConstruct = rsock(websocket),
+        uri = 'ws://localhost:8000',
+        server = websocket.Server({port: 8000});
+
+    it('should have all of the correct parameters', function() {
+      var socketInstance = reliableConstruct(uri);
+      console.log(socketInstance);
+    });
+
+  });
+});
