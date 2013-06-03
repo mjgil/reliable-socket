6afd633282c299bfaef5ad5605e9edcacb63d50d
Malcom Gilbert <malcomgilbert@gmail.com>
2013-06-03 12:22:45 -0400

    adding client request for session id

--- test/serverside.js
+++ test/serverside.js
@@ -17,10 +17,10 @@ describe('reliable-socket', function() {
   describe('reliable-socket-instance', function() {
     var reliableConstruct = rsock(websocket),
         uri = 'ws://localhost:8000',
-        server = websocket.Server({port: 8000}),
-        socketInstance = reliableConstruct(uri);
+        server = new websocket.Server({port: 8000});
 
     it('should have all of the correct parameters', function() {
+      var socketInstance = reliableConstruct(uri);
       expect(socketInstance.uri).to.be(uri);
       expect(socketInstance.reconnectTimeout).to.be(5000);
       expect(socketInstance.retryTimeout).to.be(1000);
@@ -28,12 +28,34 @@ describe('reliable-socket', function() {
       expect(socketInstance.lastSeen).to.be(0);
       expect(socketInstance.firstOpen).to.be(true);
       expect(socketInstance.firstClosed).to.be(true);
+      expect(socketInstance.sid).to.be(null);
       expect(socketInstance.send).to.be.a('function');
       expect(socketInstance.write).to.be.a('function');
       expect(socketInstance.setupSocketListeners).to.be.a('function');
       expect(socketInstance.tryToReopen).to.be.a('function');
       expect(socketInstance.checkStillClosed).to.be.a('function');
       expect(socketInstance.handleData).to.be.a('function');
+    });
+
+    describe('underlying socket listeners', function() {
+      it('should open correctly', function(done) {
+        var socketInstance = reliableConstruct(uri);
+        setTimeout(function() {
+          expect(socketInstance.readyState).to.be('open');
+          done();
+        }, 10)
+      });
+
+      it('should set session id correctly', function(done) {
+        server.on('connection', function(socket) {
+          socket.send('sessionID:123');
+        });
+        var socketInstance = reliableConstruct(uri);
+        setTimeout(function() {
+          expect(socketInstance.sid).to.be('123');
+          done();
+        }, 10)
+      });
     });
 
 
